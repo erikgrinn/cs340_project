0059a639d1d777e66b093f16960348f9a66d29dd
Erik Grinn <grinn.erik@gmail.com>
2025-03-30 19:22:12 -0400

    gh-pages pkg frontend (with base parameter in vite config), supabase pkg backend in progress. need env

--- App/backend/database/config.js
+++ App/backend/database/config.js
@@ -3,19 +3,26 @@
 // Based on: CS 340 Starter Code
 // Source URL: https://github.com/osu-cs340-ecampus/react-starter-app
 
-// Get an instance of mysql we can use in the app
-const mysql = require("mysql2");
-require("dotenv").config();
+const { createClient } = require('@supabase/supabase-js');
 
-// Create a 'connection pool' using the provided credentials
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  waitForConnections: true,
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "your_default_password",
-  database: process.env.DB_DATABASE || "your_default_database",
-}).promise();
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// Export it for use in our application
-module.exports = { pool };
+module.exports = { supabase }
+
+
+// // Get an instance of mysql we can use in the app
+// // const mysql = require("mysql2");
+// require("dotenv").config();
+
+// // Create a 'connection pool' using the provided credentials
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   waitForConnections: true,
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASSWORD || "your_default_password",
+//   database: process.env.DB_DATABASE || "your_default_database",
+// }).promise();
+
+// // Export it for use in our application
+// module.exports = { pool };
--- App/frontend/vite.config.js
+++ App/frontend/vite.config.js
@@ -12,13 +12,14 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cs340_project/',
   esbuild: {
     loader: "jsx"
   },
-  server: {
-    // Use VITE_PORT from your .env, or default to a port if not specified
-    port: parseInt(process.env.VITE_PORT, 10) || 9367,
-    host: true,
-    allowedHosts: ['classwork.engr.oregonstate.edu']
-  }
+  // server: {
+  //   // Use VITE_PORT from your .env, or default to a port if not specified
+  //   port: parseInt(process.env.VITE_PORT, 10) || 9367,
+  //   host: true,
+  //   allowedHosts: ['classwork.engr.oregonstate.edu']
+  // }
 })
